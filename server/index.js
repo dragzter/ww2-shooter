27fd3a79f9b6fb955c7895833f2b8b3869857c99
vladimir mujakovic <>
2021-08-21 00:59:55 -0400

    removing bad package

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const favicon = require("serve-favicon");
 const app = express();
 
 dotenv.config();
 
 const port = process.env.PORT;
 
-app.use(favicon("../static/favicon.ico"));
 app.use(express.static("../static/"));
 
 app.listen(port, () => {
